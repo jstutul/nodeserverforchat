c7866b9f4bafd0bbf79102871faf7eee23fbe094
jstutul <jaforsadiktutul9228@gmail.com>
2022-10-27 11:38:36 +0600

    test

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res, next) =>
 res.send("<h1>Server Running</h1>")
 );
-io.on('connection', function(socket){
-io.emit('message from server', 'message from server - it works!')
-})
-server.listen(port, () => {
+
+app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });

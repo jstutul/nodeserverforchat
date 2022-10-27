50edc1c72b6f2956979e984fba3f8d1a56c630e1
jstutul <jaforsadiktutul9228@gmail.com>
2022-10-27 11:31:04 +0600

    test

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-
-
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3001;
 
-const server = http.createServer(app);
-const users = {};
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  socket.on("new-user-join", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-join", name);
-  });
-  socket.on("send", (message) => {
-    socket.broadcast.emit("receive", {
-      message: message,
-      name: users[socket.id],
-    });
-  });
-  socket.on("speaking", (spk) => {
-    socket.broadcast.emit("speak", { spk: spk, name: users[socket.id] });
-  });
-  socket.on("disconnect", (message) => {
-    socket.broadcast.emit("leave", users[socket.id]);
-    delete users[socket.id];
-  });
-});
-
-const port = process.env.PORT || 4000;
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
+app.get('/', (req, res, next) =>
+res.send("<h1>Server Running</h1>")
+);
+io.on('connection', function(socket){
+io.emit('message from server', 'message from server - it works!')
+})
 server.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
